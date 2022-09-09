f1e56616899d948251cfada08973b10a27a9c2c8
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-09 19:49:28 -0300

    Comments and some corrections on index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ index """
 
-from itertools import count
 from api.v1.views import app_views
 from flask import jsonify
+from models.engine import file_storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
+    """returns a JSON: "status": \"OK\""""
     return (jsonify({"status": "OK"}))
 
 @app_views.route('/api/v1/stats')
 def stats():
-    count()
+    """retrieves the number of each objects by type"""
+    file_storage.count()
