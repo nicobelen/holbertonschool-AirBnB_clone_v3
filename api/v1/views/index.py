d6ead046960b24643cd59c555f4694d3222dbc79
Dak4rPrO <shinoflow111@gmail.com>
2022-09-09 19:26:14 -0300

    README & task 5 - terminar

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ index """
 
+from itertools import count
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -8,3 +9,7 @@ from flask import jsonify
 @app_views.route('/status', methods=['GET'])
 def status():
     return (jsonify({"status": "OK"}))
+
+@app_views.route('/api/v1/stats')
+def stats():
+    count()
