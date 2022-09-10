7abc11d3b4d947f7de4d3eab9d8659f7fefdf731
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-09 22:36:34 -0300

    Undoing last change and erasing old dict

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,18 +6,13 @@ from flask import Flask, jsonify
 from models import storage
 
 
-classes = {"Amenity": storage.count('Amenity'), "City": storage.count('City'),
-           "Place": storage.count('Place'), "Review": storage.count('Review'),
-           "State": storage.count('State'), "User": storage.count('User')}
-
-
 @app_views.route('/status')
 def status():
     """returns a JSON: "status": \"OK\""""
     return (jsonify({"status": "OK"}))
 
 
-@app_views.route('/api/v1/stats')
+@app_views.route('/stats')
 def stats():
     """retrieves the number of each objects by type"""
     return jsonify({"amenities": storage.count('amenities'),
