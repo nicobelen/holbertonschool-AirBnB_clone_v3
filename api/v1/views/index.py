d9f3e982e00c2a0420556d3a9102eb72c74c1a98
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-09 21:52:24 -0300

    Test on index.py/stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,16 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status')
@@ -15,4 +25,5 @@ def status():
 @app_views.route('/stats')
 def stats():
     """retrieves the number of each objects by type"""
-    storage.count()
+    for clas in classes:
+        return storage.count(clas)
