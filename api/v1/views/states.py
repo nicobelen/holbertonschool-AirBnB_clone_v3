0328e56f0e898b596456831a4f62d96bfae7a2ed
Dak4rPrO <shinoflow111@gmail.com>
2022-09-10 20:45:40 -0300

    fix and finish task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """New view for State objects that handles all default RESTFul API actions"""
 
+from crypt import methods
+from flask import Flask, jsonify
 from models import storage
+from models.state import State
+from api.v1.views import app_views
 
 
-def todict():
-    to_dict()
+@app_views.route('/states', methods=['GET'])
+def allStates():
+    """ Retrieves the list of all State objects """
+    return
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def state():
+    """ Retrieves a State object """
+    return
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state():
+    """ Deletes a State object """
+    return
