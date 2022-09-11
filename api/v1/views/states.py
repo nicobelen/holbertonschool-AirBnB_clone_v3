f7c141de3e5cb024e7794a8cf157a09a7abf9e30
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-11 18:42:52 -0300

    Added allStates, get state and delete state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """New view for State objects that handles all default RESTFul API actions"""
 
-from crypt import methods
-from flask import Flask, jsonify
+
+from os import abort
+from flask import Flask, jsonify, abort, request
 from models import storage
 from models.state import State
 from api.v1.views import app_views
@@ -11,14 +12,26 @@ from api.v1.views import app_views
 @app_views.route('/states', methods=['GET'])
 def allStates():
     """ Retrieves the list of all State objects """
-    return
+    st = []
+    for state in storage.all('State').values():
+        st.append(state.to_dict())
+    return jsonify(st)
 
 @app_views.route('/states/<state_id>', methods=['GET'])
-def state():
+def state(state_id):
     """ Retrieves a State object """
-    return
+    st = storage.get(State, state_id)
+    if (st is None):
+        abort(404)
+    else:
+        return jsonify(st.to_dict())
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
-def delete_state():
+def delete_state(state_id):
     """ Deletes a State object """
-    return
+    st = storage.get(State, state_id)
+    if (st is None):
+        abort(404)
+    storage.delete(st)
+    storage.save()
+    return jsonify({}), 200
